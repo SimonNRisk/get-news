d3803b06fd4a9413740ed09480ac7c5f53695e88
SimonNRisk <76711152+SimonNRisk@users.noreply.github.com>
2025-07-21 21:13:13 -0400

    be and fe

--- frontend/pages/api/news.ts
+++ frontend/pages/api/news.ts
@@ -1,15 +1,15 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import axios from "axios";
 
-const NEWS_API_KEY = process.env.NEWS_API_KEY;
-const NEWS_URL = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${NEWS_API_KEY}`;
+const BACKEND_URL =
+  process.env.BACKEND_NEWS_API || "http://localhost:4000/news";
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
   try {
-    const response = await axios.get(NEWS_URL);
+    const response = await axios.get(BACKEND_URL);
     res.status(200).json(response.data);
   } catch (error) {
     console.error("Failed to fetch", error);
